8e758b522811cc1764a920acd50c3d2d9d0f465a
shavity <y.shavit.mail@gmail.com>
2020-06-03 21:24:27 +0300

    אני באמצע טסט לצילינדר

---
+++ src/unittests/CylinderTests.java
@@ -0,0 +1,11 @@
+package unittests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import geometries.*;
+import primitives.*;
+
+public class CylinderTests
+{
+    Coordinate c1 = new Coordinate(1);
+}
