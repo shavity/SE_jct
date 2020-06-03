c48cd7cd7db21fe10c057f3cab3357675cdaac0f
shavity <y.shavit.mail@gmail.com>
2020-06-03 21:32:43 +0300

    אני באמצע טסט לצילינדר

--- src/unittests/CylinderTests.java
+++ src/unittests/CylinderTests.java
@@ -7,5 +7,13 @@ import primitives.*;
 
 public class CylinderTests
 {
-    Coordinate c1 = new Coordinate(1);
+    Coordinate xp = new Coordinate(1);
+    Coordinate yp = new Coordinate(2);
+    Coordinate zp = new Coordinate(3);
+    Coordinate xv = new Coordinate(4);
+    Coordinate yv = new Coordinate(5);
+    Coordinate zv = new Coordinate(6);
+
+    Point_3D p = new Point_3D(xp, yp, zp);
+    Vector v = new Vector(xv, yv, zv);
 }
